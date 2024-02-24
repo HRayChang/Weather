7a18dd0eacfb3423fc2041734e2f794914371fa2
RayChang <raychang861205@gmail.com>
2024-02-24 17:04:53 +0800

    setup WeatherManager

---
+++ Weather/Managers/WeatherManager.swift
@@ -0,0 +1,66 @@
+//
+//  WeatherManager.swift
+//  Weather
+//
+//  Created by Ray Chang on 2024/2/24.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+    
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)}&lon=\(longitude)&appid=a080cfd0f801f0bdd0ec487d219f36c9&units=metric") else { fatalError("Something Went Wrong...") }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        let (data, res) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard (res as? HTTPURLResponse)?.statusCode == 200 else { fatalError("Error fetching weather data") }
+        
+        let decodedData = try JSONDecoder().decode(ResponseBody.self, from: data)
+        
+        return decodedData
+    }
+}
+
+struct ResponseBody: Decodable {
+    var coord: CoordinateResponse
+    var weather: [WeatherResponse]
+    var main: MainResponse
+    var name: String
+    var wind: WindResponse
+    
+    struct CoordinateResponse: Decodable {
+        var lon: Double
+        var lat: Double
+    }
+    
+    struct WeatherResponse: Decodable {
+        var id: Double
+        var main: String
+        var description: String
+        var icon: String
+    }
+    
+    struct MainResponse: Decodable {
+        var temp: Double
+        var feels_like: Double
+        var temp_min: Double
+        var temp_max: Double
+        var pressure: Double
+        var humidity: Double
+    }
+    
+    struct WindResponse: Decodable {
+        var speed: Double
+        var deg: Double
+    }
+}
+
+extension ResponseBody.MainResponse {
+    var feelsLike: Double { return feels_like }
+    var tempMin: Double { return temp_min }
+    var tempMax: Double { return temp_max }
+}
