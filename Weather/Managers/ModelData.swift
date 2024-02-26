e05b1a628cab47a57f444b6d3e915cb4f2a8f39e
RayChang <raychang861205@gmail.com>
2024-02-26 14:55:21 +0800

    creat ModelData file

---
+++ Weather/Managers/ModelData.swift
@@ -0,0 +1,18 @@
+//
+//  ModelData.swift
+//  Weather
+//
+//  Created by Ray Chang on 2024/2/24.
+//
+
+import SwiftUI
+
+struct ModelData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelData()
+}
