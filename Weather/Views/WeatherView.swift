8d1dee978cce390090261f2e921a4eb001c45251
RayChang <raychang861205@gmail.com>
2024-02-26 15:19:50 +0800

    create WeatherView file

---
+++ Weather/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Ray Chang on 2024/2/26.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
