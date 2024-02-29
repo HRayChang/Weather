8385d1ebe60571b46e9b698885226e85f40dbf74
RayChang <raychang861205@gmail.com>
2024-03-01 00:33:01 +0800

    setup WeatherView UI

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -10,9 +10,34 @@ import SwiftUI
 struct WeatherView: View {
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Stockholm,\nSweden")
+                .font(.system(size: 36, weight: .semibold))
             
+            Text("Tue, Jan 30")
+                .font(.system(size: 20))
+                .opacity(0.5)
+            
+            HStack {
+                Image("rain")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                
+                VStack {
+                    Text("19")
+                        .font(.system(size: 86, weight: .semibold))
+                    
+                    Text("Rainy")
+                        .font(.system(size: 26))
+                }
+                .padding(.leading)
+                
+                Spacer()
+            }
+            .padding()
         }
+        .padding()
         .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
         .background(LinearGradient(colors: [Color("light"), Color("dark")], startPoint: .topLeading, endPoint: .bottomTrailing))
     }
